94c1f9b412a58ace81f9e0291d5a5e028c7bc480
TrotskyuYurii <tro2001@gmail.com>
2024-05-03 10:22:58 +0300

    fix2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { refreshUser } from "./redux/auth/operations";
 import Layout from "./components/Layout/Layout";
 import RestrictedRoute from "./components/RestrictedRoute/RestrictedRoute";
@@ -10,17 +10,22 @@ import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import ContactsPage from "./pages/ContactsPage/ContactsPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import { selectIsRefreshing } from "./redux/auth/selectors";
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const token = localStorage.getItem('token'); 
+    // const token = localStorage.getItem('token'); 
 
-    if (token) {
+    // if (token) {
         dispatch(refreshUser());
-    }
+    // }
 }, [dispatch]);
+
+  const isRefreshing = useSelector(selectIsRefreshing);
+  if (isRefreshing) {<div>Loading...</div>}
+
 
   return (
     <Layout>
